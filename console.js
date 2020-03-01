55e3bd2968944b5efc8b98acf0f0c2311184539f
Zack Hoffmann <zachary.hoffmann@gmail.com>
2020-03-01 16:04:39 -0500

    Testing out simple JS console.

---
+++ console.js
@@ -0,0 +1,14 @@
+function input(e) {
+    console.log(e);
+    const form = e.target;
+    const inField = form.elements.input;
+    const promptText = form.closest(".prompt").querySelector(".text").innerText;
+    const newP = document.createElement("p");
+    newP.innerText = promptText + inField.value;
+    inField.value = "";
+    form.closest(".wrapper").querySelector(".scroll").appendChild(newP);
+    window.scrollTo(0,document.body.scrollHeight);
+    e.preventDefault();
+}
+
+document.querySelectorAll(".input-form").forEach((f) => f.addEventListener("submit", input));
