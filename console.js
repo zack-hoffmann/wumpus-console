0a81dc0f9743425743ddf5787103c78f32780908
Zack Hoffmann <zachary.hoffmann@gmail.com>
2020-03-01 22:12:56 -0500

    Fighting with how to handle line width...

--- console.js
+++ console.js
@@ -1,14 +1,19 @@
-function input(e) {
-    console.log(e);
-    const form = e.target;
-    const inField = form.elements.input;
-    const promptText = form.closest(".prompt").querySelector(".text").innerText;
-    const newP = document.createElement("p");
-    newP.innerText = promptText + inField.value;
-    inField.value = "";
-    form.closest(".wrapper").querySelector(".scroll").appendChild(newP);
-    window.scrollTo(0,document.body.scrollHeight);
-    e.preventDefault();
-}
+(function (params) {
+    function input(e) {
+        console.log(e);
+        const form = e.target;
+        const inField = form.elements.input;
+        const promptText = form.closest(".prompt").querySelector(".text").innerText;
+        const newP = document.createElement("p");
+        newP.innerText = promptText + inField.value;
+        inField.value = "";
+        form.closest(".wrapper").querySelector(".scroll").appendChild(newP);
+        window.scrollTo(0, document.body.scrollHeight);
+        e.preventDefault();
+    }
 
-document.querySelectorAll(".input-form").forEach((f) => f.addEventListener("submit", input));
+    function setLineWidth(width) {
+    }
+
+    document.querySelectorAll(".input-form").forEach((f) => f.addEventListener("submit", input));
+})({});
