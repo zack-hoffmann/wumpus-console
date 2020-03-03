440314b28df7db85563ddd52cdb2e0a440aeb9d7
Zack Hoffmann <zachary.hoffmann@gmail.com>
2020-03-02 21:44:07 -0500

    Working line widths, auto-focus.

--- console.js
+++ console.js
@@ -1,19 +1,45 @@
 (function (params) {
+    
+    function write(t, parent) {
+        const newP = document.createElement("p");
+        newP.innerText = t;
+        parent.appendChild(newP);
+    }
+
     function input(e) {
-        console.log(e);
         const form = e.target;
         const inField = form.elements.input;
-        const promptText = form.closest(".prompt").querySelector(".text").innerText;
-        const newP = document.createElement("p");
-        newP.innerText = promptText + inField.value;
+        const promptText = form.querySelector("label").innerText;
+        write(promptText + inField.value, form.closest(".wrapper").querySelector(".scroll"));
         inField.value = "";
-        form.closest(".wrapper").querySelector(".scroll").appendChild(newP);
         window.scrollTo(0, document.body.scrollHeight);
         e.preventDefault();
     }
 
     function setLineWidth(width) {
+        document.querySelector(".wrapper").style.width = width + "ch";
+        const l = document.querySelector("label").innerText.length;
+        document.querySelector("input").setAttribute("maxlength", width - l);
+        document.querySelector("input").style.width = (width - l) + "ch";
+    }
+
+    function stagePrompt(p) {
+        params.stagedPrompt = p;
+    }
+
+    function updatePrompt() {
+        document.querySelector("label").innerText = params.stagedPrompt;
+    }
+
+    function inputFocus(e) {
+        if (e.target.nodeName === "HTML" || e.target.nodeName === "BODY") {
+            document.querySelector("input").focus();
+        }
     }
 
     document.querySelectorAll(".input-form").forEach((f) => f.addEventListener("submit", input));
+    window.addEventListener("click", inputFocus);
+    stagePrompt("test-prompt> ");
+    updatePrompt();
+    setLineWidth(80);
 })({});
